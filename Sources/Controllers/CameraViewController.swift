e94a1c365d23432511b137a237d10b4aae9cab57
Raul Ramirez <raul.ramtap@gmail.com>
2023-04-02 22:53:00 -0600

    Rotation issue

--- Sources/Controllers/CameraViewController.swift
+++ Sources/Controllers/CameraViewController.swift
@@ -405,8 +405,10 @@ private extension CameraViewController {
     print(" *-*-*-*-*-*- BOUNDS INFO -*-*-*-*-*-* ")
     print("View.layer.bounds: height=\(view.layer.bounds.height), width=\(view.layer.bounds.width)")
     print("UIScreen.main.bounds: height=\(UIScreen.main.bounds.height), width=\(UIScreen.main.bounds.width)")
-      print("UIApplication.shared.windows.first: height=\(UIApplication.shared.windows.first!.bounds.height), width=\(UIApplication.shared.windows.first!.bounds.width)")
-      
+    print("UIApplication.shared.windows.first: height=\(UIApplication.shared.windows.first!.bounds.height), width=\(UIApplication.shared.windows.first!.bounds.width)")
+    print("UIApplication.shared.windows.first.safeAreaLayoutGuide: height=\(UIApplication.shared.windows.first!.safeAreaLayoutGuide.layoutFrame.height), width=\(UIApplication.shared.windows.first!.safeAreaLayoutGuide.layoutFrame.width)")
+        
+    
     videoPreviewLayer.frame = view.layer.bounds
 
     if let connection = videoPreviewLayer.connection, connection.isVideoOrientationSupported {
